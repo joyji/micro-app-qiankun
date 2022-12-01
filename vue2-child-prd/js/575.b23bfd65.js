66ac94122aed970e2e64085db3062baa83203f9a
jiying <136969071@qq.com>
2022-12-01 15:22:12 +0800

    增加vite deploy

---
+++ vue2-child-prd/js/575.b23bfd65.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackJsonp_@qiankun/vue2-child"]=self["webpackJsonp_@qiankun/vue2-child"]||[]).push([[575],{92575:function(n,u,e){e.r(u),e.d(u,{default:function(){return c}});var t=function(){var n=this,u=n._self._c;return u("router-view")},r=[],i={name:"Layout"},l=i,s=e(36432),a=(0,s.Z)(l,t,r,!1,null,"28413651",null),c=a.exports}}]);
+//# sourceMappingURL=575.b23bfd65.js.map
---
+++ vue2-child-prd/js/839.81308c2e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackJsonp_@qiankun/vue2-child"]=self["webpackJsonp_@qiankun/vue2-child"]||[]).push([[839],{53839:function(e,n,u){u.r(n),u.d(n,{default:function(){return r}});var i=function(){var e=this,n=e._self._c;return n("div",[e._v("vue2 CoexistView")])},s=[],t={name:"CoexistView"},a=t,l=u(36432),c=(0,l.Z)(a,i,s,!1,null,"aba1ee20",null),r=c.exports}}]);
+//# sourceMappingURL=839.81308c2e.js.map
